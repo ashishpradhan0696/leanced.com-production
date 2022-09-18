5865ca7505c50252b5c0e676fdb1005009815952
Ashish <ashish.pradhan0696@gmail.com>
2022-09-18 19:04:55 +0530

    Fixing peer dependency issues:Moving back to react and reactdom @17.0.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom'; //React 17.0.0
 import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
